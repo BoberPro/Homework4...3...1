ce4c0ff9b9401830ce4158b70c3fb5d242440b29
BoberPro <34961963+BoberPro@users.noreply.github.com>
2018-02-24 21:07:11 +0500

    Quarty
    quart

---
+++ app/src/main/java/com/example/samsung/homework41/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.samsung.homework41;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ListActivity {
+
+    String[] myArr = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> monthAdapter =
+                new ArrayAdapter<String>(this,                                                                                                                                           android.R.layout.simple_list_item_1, myArr);
+
+        setListAdapter(monthAdapter);
+        myArr[11] = "November";
+        monthAdapter.notifyDataSetInvalidated();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String month = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, month, Toast.LENGTH_SHORT).show();
+
+    }
+}
